204533a2779d8be68265a3f74e85bae65e8bc268
VND2004 <vunhuduc2004@gmail.com>
2024-04-25 11:21:46 +0700

    Passing parameter

--- Lab 03/AimsProject/src/DigitalVideoDisc.java
+++ Lab 03/AimsProject/src/DigitalVideoDisc.java
@@ -48,16 +48,39 @@ public class DigitalVideoDisc {
 	public String getTitle() {
 		return title;
 	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 	public String getCategory() {
 		return category;
 	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
 	public String getDirector() {
 		return director;
 	}
+	
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
 	public int getLength() {
 		return length;
 	}
+	
+	public void setLength(int length) {
+		this.length = length;
+	}
+
 	public float getCost() {
 		return cost;
+	}
+	
+	public void setCost(float cost) {
+		this.cost = cost;
 	}
 }
